fc1c76ffccdf66c9a67439bdf6449c23620c9f68
ZOukoO <walid.moustaoui@gmail.com>
2015-01-05 11:35:32 +0100

    Update Eleve.java

--- Eleve.java
+++ Eleve.java
@@ -1,4 +1,13 @@
 
 public class Eleve extends Personne {
-	
+	String[] eleve=new String[20];
+	int compteur=0;
+	public Eleve(String nom, String prenom){
+		this.eleve[compteur]=nom+" "+prenom;
+		this.compteur++;
+		
+	}
+	public String toString(){
+		return(" L'eleve "+this.eleve[0]+" a été ajouté");
+	}
 }
