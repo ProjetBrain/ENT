5e506f04c6c99780656e69ad45543151a4bb2291
ZOukoO <walid.moustaoui@gmail.com>
2015-01-05 11:36:08 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 	
-	Eleve e = new Eleve();
+	Eleve e;
 	Professeur p = new Professeur();
 	Matiere m = new Matiere();
 	Note n = new Note();
@@ -13,14 +13,22 @@ public class Main {
 		System.out.println("Creer nouvel eleve : 1");
 		System.out.println("Creer nouveau bulletin : 2");
 		System.out.println("Modifier bulletin existant: 3");
-		String str = sc.nextLine();
+		int str = sc.nextInt();
 		
-		if(str==1)
+		if(str==1){
+			sc.nextLine();
 			System.out.println("Nom de l'eleve : ");
-			
-			
+			String nEleve = sc.nextLine();
+			System.out.println("Prenom de l'eleve : ");
+			String pEleve = sc.nextLine();
+			e= new Eleve(nEleve, pEleve);
+			System.out.println(e.toString());
+		}
+		else
+			System.out.println("probleme");
 			
 		
 	
 	}
+
 }
