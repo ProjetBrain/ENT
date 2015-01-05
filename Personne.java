20ad95898cc655d082067f3b8f1d3fcac6a77c81
ZOukoO <walid.moustaoui@gmail.com>
2015-01-05 10:13:26 +0100

    Create Personne.java

---
+++ Personne.java
@@ -0,0 +1,25 @@
+
+public class Personne {
+	
+	private String nom;
+	private String prenom;
+	
+	public Personne(){}
+	
+	public String getNom(){
+		return this.nom;
+	}
+	
+	public String getPrenom(){
+		return this.prenom;
+	}
+	
+	public void setNom(String nom){
+		this.nom = nom;
+	}
+	
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+}
