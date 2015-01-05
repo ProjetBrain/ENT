4d0f42764ed2011469faa5bc90ecf90a4f174cf9
ZOukoO <walid.moustaoui@gmail.com>
2015-01-05 12:05:01 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -24,6 +24,54 @@ public class Main {
 			e= new Eleve(nEleve, pEleve);
 			System.out.println(e.toString());
 		}
+		
+		else if(str==2){
+		sc.nextLine();
+
+		String tabBuletin[] = new String[25];
+		int compt=0;
+
+		System.out.println("Entrez le nom de l'eleve :");
+		String a = sc.nextLine();
+		tabBuletin[compt]=a+" ";
+		
+		System.out.println("Entrez le prenom de l'eleve :");
+		String b = sc.nextLine();
+		tabBuletin[compt]+=b+" ";
+
+		System.out.println("Entrez la note en Math :");
+		String Math = sc.nextLine();
+		tabBuletin[compt]+=Math+" ";
+
+		System.out.println("Entrez la note en Français :");
+		String Français = sc.nextLine();
+		tabBuletin[compt]+=Français+" ";
+
+		System.out.println("Entrez la note en Histoire :");
+		String Histoire = sc.nextLine();
+		tabBuletin[compt]+=Histoire+" ";
+
+		System.out.println("Entrez la note en EPS :");
+		String EPS = sc.nextLine();
+		tabBuletin[compt]=EPS+" ";
+
+		System.out.println("Entrez la note en SVT");
+		String SVT = sc.nextLine();
+		tabBuletin[compt]+=SVT+" ";
+
+		System.out.println("Entrez la note en Chimie");
+		String Chimie = sc.nextLine();
+		tabBuletin[compt]+=Chimie;
+		
+		compt++;
+		}
+		else if(str==3){
+		sc.nextLine();
+		System.out.println("Saisir la nouvelle matiere : ");
+		String matiere = sc.nextLine();
+		m.setNom(matiere);
+		System.out.println(m.toString());
+		}
 		else
 			System.out.println("probleme");
 			
