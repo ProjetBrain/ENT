e12b63ee5a49922ba2de5cc937f95038e317b634
ZOukoO <walid.moustaoui@gmail.com>
2015-01-05 10:15:14 +0100

    Create Matiere.java

---
+++ Matiere.java
@@ -0,0 +1,24 @@
+
+public class Matiere {
+
+	private String nom;
+	
+	private float coefficient;
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public float getCoefficient() {
+		return coefficient;
+	}
+	
+	public void setCoefficient(float coefficient) {
+		this.coefficient = coefficient;
+	}
+	
+}
