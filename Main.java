09437515d9d9e41223fabeb50a1fa5083fc30a40
ZOukoO <walid.moustaoui@gmail.com>
2015-01-05 10:14:09 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Main {
+	
+	Eleve e = new Eleve();
+	Professeur p = new Professeur();
+	Matiere m = new Matiere();
+	Note n = new Note();
+	
+	public void inter(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Creer nouvel eleve : 1");
+		System.out.println("Creer nouveau bulletin : 2");
+		System.out.println("Modifier bulletin existant: 3");
+		String str = sc.nextLine();
+		
+		if(str==1)
+			System.out.println("Nom de l'eleve : ");
+			
+			
+			
+		
+	
+	}
+}
